793fa36de0a77c56f0d9f4323aaaddb4c12c0d7b
SDatta <sodatta@gmail.com>
2019-08-02 15:55:11 +0530

    Added Project Lombok. Closes #51

--- pet-clinic-data/src/main/java/com/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/petclinic/model/Person.java
@@ -1,11 +1,23 @@
 package com.petclinic.model;
 
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person extends BaseEntity{
+
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     @Column(name = "first_name")
     private String firstName;
@@ -13,21 +25,4 @@ public class Person extends BaseEntity{
     @Column(name = "last_name")
     private String lastName;
 
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
